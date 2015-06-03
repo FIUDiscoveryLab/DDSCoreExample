79745924412699b5e00c1a7275f82dba0265aae2
Irvin Steve Cardenas <irvsteve@gmail.com>
2015-06-03 16:04:15 -0400

    Add new files

--- src/multicast/HelloSubscriber.java
+++ src/multicast/HelloSubscriber.java
@@ -1,3 +1,4 @@
+package multicast;
 // ****************************************************************************
 //         (c) Copyright, Real-Time Innovations, All rights reserved.       
 //                                                                          
@@ -31,12 +32,39 @@ public class HelloSubscriber extends DataReaderAdapter {
     private static boolean shutdown_flag = false;
 
     public static final void main(String[] args) {
+    	List<String> fileNames = new ArrayList<String>();
+        //BufferedReader reader = new BufferedReader(new FileReader(Config.QOS_FILE_PATH));
+        //filenames.add("base_profile_multicast.xml");
+        //filenames.add("MaxMulticast.xml");
+    	fileNames.add("MaxMulticast.xml");
+    	
+        // Create the DDS Domain participant on domain ID 0
+		DomainParticipantFactoryQos factoryQos = 
+				new DomainParticipantFactoryQos();
+		DomainParticipantFactory.get_instance().get_qos(factoryQos);
+		factoryQos.profile.url_profile.setMaximum(fileNames.size());
+		for (int i = 0; i < fileNames.size(); i++) {
+			
+			factoryQos.profile.url_profile.add(fileNames.get(i));
+		}
 
+		DomainParticipantFactory.get_instance().set_qos(factoryQos);
+		
+		DomainParticipant participant = 
+				DomainParticipantFactory.get_instance()
+				.create_participant_with_profile(
+									0,
+									"BasicAEONProfile", 
+									 "MaxThroughputMulticast", 
+									null, 
+									StatusKind.STATUS_MASK_NONE);
+    	/*
         DomainParticipant participant = DomainParticipantFactory.get_instance().create_participant(
                 0, // Domain ID = 0
                 DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT, 
                 null, // listener
                 StatusKind.STATUS_MASK_NONE);
+        */
         
         if (participant == null) {
             System.err.println("Unable to create domain participant");
